628cdddb6ada6ef6983f0991656692e8150d9aa0
Saiswarna846 <saisumanth437@gmail.com>
2025-06-09 11:11:00 +0530

    Add files via upload

---
+++ src/utils/Trails/trailImages.js
@@ -0,0 +1,23 @@
+import athirapally from "./Trail_Images/Athirapally.png";
+import chembra from "./Trail_Images/Chembra.png";
+import peechi from "./Trail_Images/Peechi.png";
+import parambikulam from "./Trail_Images/Parambikulam.png";
+import nelliyampathy from "./Trail_Images/Nelliyampathy.png";
+import chokramudi from "./Trail_Images/Chokramudi.png";
+import dhoni from "./Trail_Images/Dhoni.png";
+import chimmony from "./Trail_Images/Chimmony.png";
+import Nature from "../../images/nature1.jpg"; // Default image
+
+const trailImages = {
+    "athirapally": athirapally,
+    "chembra": chembra,
+    "peechi": peechi,
+    "parambikulam": parambikulam,
+    "nelliyampathy": nelliyampathy,
+    "chokramudi": chokramudi,
+    "dhoni": dhoni,
+    "chimmony":chimmony
+};
+
+export default trailImages;
+export { Nature }; // Export default fallback image
